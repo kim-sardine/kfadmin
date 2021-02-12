adbdef6619c21d2b04827a14e0085d550bb61ade
kim-sardine <kim.sardine@gmail.com>
2021-02-12 23:32:51 +0900

    Print users with table

--- cmd/listUser.go
+++ cmd/listUser.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
-	"fmt"
+	"os"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/spf13/cobra"
 
 	"github.com/kim-sardine/kfadmin/client/manifest"
-	"github.com/spf13/cobra"
 )
 
 // listUserCmd list all user
@@ -19,10 +21,19 @@ var listUserCmd = &cobra.Command{
 		}
 
 		dc := manifest.UnmarshalDexConfig(cm.Data["config.yaml"])
+
+		row := make([]table.Row, len(dc.StaticPasswords))
 		for i, user := range dc.StaticPasswords {
-			fmt.Println(i+1, user.Email)
+			row = append(row, table.Row{i + 1, user.Email})
 		}
-		fmt.Printf("Total : %d users\n", len(dc.StaticPasswords))
+
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"#", "User Email"})
+		t.AppendRows(row)
+		t.AppendFooter(table.Row{"Total", len(dc.StaticPasswords)})
+		t.Render()
+
 	},
 }
 
