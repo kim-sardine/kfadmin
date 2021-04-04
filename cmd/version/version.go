ea2630781751591e8939b92c51a9e1ff09499846
kim-sardine <kim.sardine@gmail.com>
2021-04-04 23:03:33 +0900

    use stream output

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -11,13 +11,26 @@ type VersionString string
 
 var CurrentVersion VersionString = "v21.3.31"
 
+type Options struct {
+	clioption.IOStreams
+}
+
+// NewOptions returns initialized Options
+func NewOptions(ioStreams clioption.IOStreams) *Options {
+	return &Options{
+		IOStreams: ioStreams,
+	}
+
+}
+
 func NewCmdVersion(ioStreams clioption.IOStreams) *cobra.Command {
+	o := NewOptions(ioStreams)
 
 	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Print version",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println(CurrentVersion)
+			fmt.Fprintf(o.Out, "Version: %s\n", CurrentVersion)
 		},
 	}
 
