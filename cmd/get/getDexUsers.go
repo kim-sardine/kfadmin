96a448b7831f11ce45b665c0083c9938dadb8735
kim-sardine <kim.sardine@gmail.com>
2021-04-15 23:57:25 +0900

    Add GetDexUserOptions

--- cmd/get/getDexUsers.go
+++ cmd/get/getDexUsers.go
@@ -9,39 +9,60 @@ import (
 	"github.com/kim-sardine/kfadmin/client"
 	"github.com/kim-sardine/kfadmin/client/manifest"
 	"github.com/kim-sardine/kfadmin/clioption"
+	"github.com/kim-sardine/kfadmin/cmd/util"
 )
 
+type GetDexUserOptions struct {
+	KfClient *client.KfClient
+	clioption.IOStreams
+}
+
+// NewGetDexUserOptions returns initialized Options
+func NewGetDexUserOptions(c *client.KfClient, ioStreams clioption.IOStreams) *GetDexUserOptions {
+	return &GetDexUserOptions{
+		KfClient:  c,
+		IOStreams: ioStreams,
+	}
+
+}
+
 func NewCmdGetDexUsers(c *client.KfClient, ioStreams clioption.IOStreams) *cobra.Command {
+	o := NewGetDexUserOptions(c, ioStreams)
 
 	cmd := &cobra.Command{
 		Use:   "dex-users",
 		Short: "Print all dex users",
 		Long:  `TBU`,
 		Run: func(cmd *cobra.Command, args []string) {
-			cm, err := c.GetDex()
-			if err != nil {
-				panic(err)
-			}
-
-			dc, err := manifest.UnmarshalDexDataConfig(cm.Data["config.yaml"])
-			if err != nil {
-				panic(err)
-			}
-
-			row := make([]table.Row, len(dc.StaticPasswords))
-			for i, user := range dc.StaticPasswords {
-				row = append(row, table.Row{i + 1, user.Email})
-			}
-
-			t := table.NewWriter()
-			t.SetOutputMirror(os.Stdout)
-			t.AppendHeader(table.Row{"#", "User Email"})
-			t.AppendRows(row)
-			t.AppendFooter(table.Row{"Total", len(dc.StaticPasswords)})
-			t.Render()
-
+			util.CkeckErr(o.Run())
 		},
 	}
 
 	return cmd
+}
+
+func (o *GetDexUserOptions) Run() error {
+	cm, err := o.KfClient.GetDex()
+	if err != nil {
+		return err
+	}
+
+	dc, err := manifest.UnmarshalDexDataConfig(cm.Data["config.yaml"])
+	if err != nil {
+		return err
+	}
+
+	row := make([]table.Row, len(dc.StaticPasswords))
+	for i, user := range dc.StaticPasswords {
+		row = append(row, table.Row{i + 1, user.Email})
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"#", "User Email"})
+	t.AppendRows(row)
+	t.AppendFooter(table.Row{"Total", len(dc.StaticPasswords)})
+	t.Render()
+
+	return nil
 }
